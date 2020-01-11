24cd42b13238813029dd01f515e5fe7315f9faf9
pawelkulig1 <pawelkulig1@gmail.com>
2020-01-11 17:32:01 +0100

    fixed server

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, requests
+from flask import Flask, request
 from flask_cors import CORS
 app = Flask(__name__)
 
@@ -9,14 +9,17 @@ def hello():
     return """<script> 
                 function send_ajax(){
                     var xhr = new XMLHttpRequest();
-                    xhr.open('GET', 'http://127.0.0.1:5000');
+                    xhr.open('GET', 'http://127.0.0.1:5000/search?test=123');
                     xhr.send();
                 }
             </script>
             <div onclick='send_ajax()' style='width:100px;height:100px;background-color:red'></div>"""
 
-@app.route('/search', methods=['GET']):
-    print(requests.args)
+
+@app.route('/search', methods=['GET'])
+def search():
+    print(request.args['test'])
+    return "search response..."
     
 if __name__ == '__main__':
     app.run()
