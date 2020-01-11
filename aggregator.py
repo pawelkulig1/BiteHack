0137d62707057a4078de030581cd7e57fe8a1908
Jakub Mojsiejuk <cmk734@motorolasolutions.com>
2020-01-11 19:33:17 +0100

    adding db

--- aggregator.py
+++ aggregator.py
@@ -13,14 +13,16 @@ class Aggregator:
         filenames = [os.path.join(self.src, fn) for fn in os.listdir(self.src)]
         statistics = defaultdict(list)
         for filename in filenames:
-            tags, desc = self.SOC.parse_job_posting(filename)
-            if tags:
-                statistics['tags'].append(list(set(tags)))
-                if len(tags) > 20:
-                    print(tags, desc)
-                for k, v in desc.items():
-                    statistics[k].append(v)
-
+            try:
+                tags, desc = self.SOC.parse_job_posting(filename)
+                if tags:
+                    statistics['tags'].append(list(set(tags)))
+                    if len(tags) > 20:
+                        print(tags, desc)
+                    for k, v in desc.items():
+                        statistics[k].append(v)
+            except Exception as e:
+                print(f"Failed {filename}")
         df = pd.DataFrame.from_dict(statistics)
         df.to_csv('db.csv')
 
